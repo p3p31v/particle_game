d8328cc2333712e296e771740e795966adcec6ae
Jose <100155167+p3p31v@users.noreply.github.com>
2022-11-26 17:01:30 +0100

    Add files via upload

--- particles.py
+++ particles.py
@@ -15,10 +15,13 @@ car_width = 73
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Particles in da house')
 clock = pygame.time.Clock()
+obstacle = pygame.Rect(400,200,80,80)
 
 carImg = pygame.image.load('circle.png')
+rect = carImg.get_rect()
 def car(x,y):
-    gameDisplay.blit(carImg,(x,y))
+    gameDisplay.blit(carImg,rect)
+
 
 
 def text_objects(text, font):
@@ -45,11 +48,11 @@ def crash():
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
-    x1 = 0.2*x
-    y1 = 0.2*y
+   # x1 = 0.2*x
+   # y1 = 0.2*y
     x_change = 0
     y_change = 0
-
+    y1_change = 0
     gameExit = False
 
     while not gameExit:
@@ -59,7 +62,7 @@ def game_loop():
                 pygame.quit()
                 quit()
             y_change = 1
-            y1_change = 1
+           # y1_change = y1_change + 0.3
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
@@ -73,18 +76,19 @@ def game_loop():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
-        x += x_change
-        y += y_change
-        y1 += y1_change
-
+        rect.x += x_change
+        rect.y += y_change
+       # rect.y1 += y1_change
         gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay, (0,0,0),obstacle,4)
         car(x,y)
-        car(x1,y1)
-
+       # car(x1,y1)
         if x > display_width - car_width or x < 0:
             crash()
-            
-        
+        if rect.colliderect(obstacle):
+            pygame.draw.rect(gameDisplay,(255,0,0),rect,4)
+            x = 0
+             
         pygame.display.update()
         clock.tick(60)
 
